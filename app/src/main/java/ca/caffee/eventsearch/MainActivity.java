8484df24300a133dc61bc028374f1013ea10ffa4
Michal Tajchert <mtajchert@gmail.com>
2017-06-24 13:59:14 -0400

    Gitignore for idea module.

--- app/src/main/java/ca/caffee/eventsearch/MainActivity.java
+++ app/src/main/java/ca/caffee/eventsearch/MainActivity.java
@@ -29,7 +29,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     });
 
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    ActionBarDrawerToggle toggle =
+        new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
     drawer.addDrawerListener(toggle);
     toggle.syncState();
 
